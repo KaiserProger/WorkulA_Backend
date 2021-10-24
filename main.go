f1828145520bd1a5c4b8a5b7fee5b93ff094c313
KaiserProger <kaisergrobe@gmail.com>
2021-10-24 20:57:10 +0700

    Update main.go

--- main.go
+++ main.go
@@ -6,14 +6,15 @@ import (
 	"workula/db"
 	"workula/handlers"
 
-	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo"
 )
 
 func main() {
 	db.InitDB()
 	server := echo.New()
 	group := server.Host("/main", echo.WrapMiddleware())
-	server.POST("/auth/signin", handlers.SignIn)
+	group.
+		server.POST("/auth/signin", handlers.SignIn)
 	server.POST("/auth/signup", handlers.SignUp)
 	server.POST("/connect", handlers.Connect)
 	log.Fatal(server.Start(":" + string(os.Getenv("PORT"))))
